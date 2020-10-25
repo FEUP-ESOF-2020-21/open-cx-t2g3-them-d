df3b035438a060a84184d5c4bbce1d5ecc76167c
Diana Freitas <up201806230@fe.up.pt>
2020-10-25 13:28:34 +0000

    Starting Profile (#1) - Dropdown District

--- smartcon_app/lib/main.dart
+++ smartcon_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_formfield/dropdown_formfield.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -63,11 +64,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -90,29 +86,165 @@ class _MyHomePageState extends State<MyHomePage> {
               // MARGINS
               Padding(
                 padding:  EdgeInsets.only(
-                  left: MediaQuery.of(context).size.width * 0.05,
-                  right: MediaQuery.of(context).size.width * 0.05,
+                  left: MediaQuery.of(context).size.width * 0.08,
+                  right: MediaQuery.of(context).size.width * 0.08,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget> [
+                    Container(
+                      child: Text("A Title", style: Theme.of(context).textTheme.headline2,),
+                      alignment: Alignment.topLeft,
+                    ),
+                    Container(
+                      child: Text("A SUBTITLE", style: Theme.of(context).textTheme.headline3,),
+                      alignment: Alignment.topLeft,
+                    ),
+                    RaisedButton(
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Profile()),
+                        );
+                      },
+                      child: Text('Conference Suggestions'),
+                    )
+                  ],
                 ),
               ),
-              // CONTENT
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget> [
-                  Container(
-                    child: Text("A Title", style: Theme.of(context).textTheme.headline2,),
-                    alignment: Alignment.topLeft,
-                  ),
-                  Container(
-                    child: Text("A SUBTITLE", style: Theme.of(context).textTheme.headline3,),
-                    alignment: Alignment.topLeft,
-                  ),
-                ],
-              ),
-              ],
+            ]
             )
           ],
         )
       ),
+    );
+  }
+}
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  String _myDistrict;
+  String _myDistrictResult;
+  final districtKey = new GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _myDistrict = '';
+    _myDistrictResult = '';
+  }
+
+  _saveForm() {
+    var form = districtKey.currentState;
+    if (form.validate()) {
+      form.save();
+      setState(() {
+        _myDistrictResult = _myDistrict;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            children: <Widget> [
+              // HEADER IMAGE (100%)
+              Row(
+                  children: <Widget> [
+                    Expanded(
+                      child: Image.asset(
+                        'images/pageHeader.png',
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                  ]
+              ),
+              // CONTENT ROW
+              Row(
+                children: <Widget> [
+                  // MARGINS
+                  Padding(
+                    padding:  EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.08,
+                      right: MediaQuery.of(context).size.width * 0.08,
+                    ),
+                    child:
+                    // CONTENT
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget> [
+                          Row(
+                              children: [Text("Your Profile", style: Theme.of(context).textTheme.headline2,),]
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.84,
+                            child: Form(
+                              key: districtKey,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: DropDownFormField(
+                                      titleText: 'District',
+                                      hintText: 'Choose a district',
+                                      value: _myDistrict,
+                                      onSaved: (value) {
+                                        setState(() {
+                                          _myDistrict = value;
+                                        });
+                                      },
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _myDistrict = value;
+                                        });
+                                      },
+                                      dataSource: [
+                                        {
+                                          "display": "Porto",
+                                          "value": "Porto",
+                                        },
+                                        {
+                                          "display": "Aveiro",
+                                          "value": "Aveiro",
+                                        },
+                                        {
+                                          "display": "Lisboa",
+                                          "value": "Lisboa",
+                                        },
+                                        {
+                                          "display": "Viana do Castelo",
+                                          "value": "Viana do Castelo",
+                                        },
+                                        {
+                                          "display": "Faro",
+                                          "value": "Faro",
+                                        },
+                                      ],
+                                      textField: 'display',
+                                      valueField: 'value',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ]
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
     );
   }
 }
