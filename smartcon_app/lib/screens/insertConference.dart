15bc5fbad0c3a7cf282dc937310a2c2fd6c8bc60
Mariana Ramos <7marianaramos@gmail.com>
2020-11-06 20:18:03 +0000

    insert conference screen done #7

--- smartcon_app/lib/screens/insertConference.dart
+++ smartcon_app/lib/screens/insertConference.dart
@@ -1,108 +1,6 @@
-/*import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-class insertConference extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-        children: <Widget>[
-          Row(children: <Widget>[
-            Stack(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Image.asset('images/pageHeader.png', fit: BoxFit.fill),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(
-                      left: MediaQuery.of(context).size.width * 0.08,
-                      top: MediaQuery.of(context).size.width * 0.08,
-                    ),
-                    child: RaisedButton(
-                      onPressed: () async {
-                        MaterialPageRoute(
-                            builder: (context) => insertConference());
-                      },
-                      color: Color(0xFF6E96EF),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "SIGN OUT",
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                      ),
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                    )),
-              ],
-            ),
-          ]),
-          // CONTENT ROW
-          Row(children: <Widget>[
-            // MARGINS
-            Padding(
-              padding: EdgeInsets.only(
-                left: MediaQuery.of(context).size.width * 0.08,
-                right: MediaQuery.of(context).size.width * 0.08,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      "Insert Conference",
-                      style: Theme.of(context).textTheme.headline2,
-                    ),
-                    alignment: Alignment.topLeft,
-                  ),
-                  Container(
-                    child: Text(
-                      "A SUBTITLE",
-                      style: Theme.of(context).textTheme.headline3,
-                    ),
-                    alignment: Alignment.topLeft,
-                  ),
-                  RaisedButton(
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          side: BorderSide(color: Colors.black26, width: 2)),
-                      child: Text('SAVE',
-                          style: TextStyle(
-                            color: Colors.black38,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w700,
-                            fontFamily: 'Rubik',
-                          )),
-                      onPressed: () async {
-                        MaterialPageRoute(
-                            builder: (context) => insertConference());
-                      }),
-                  Container(
-                    child: Text("Some normal text",
-                        style: TextStyle(
-                            fontFamily: 'Rubik',
-                            color: Colors.black87,
-                            fontSize: 17,
-                            fontWeight: FontWeight.w400)),
-                    alignment: Alignment.topLeft,
-                  ),
-                ],
-              ),
-            ),
-          ])
-        ],
-      )),
-    );
-  }
-}*/
-
 import 'package:flutter/material.dart';
+
+import 'package:dropdown_formfield/dropdown_formfield.dart';
 
 class insertConference extends StatefulWidget {
   @override
@@ -113,17 +11,24 @@ class insertConference extends StatefulWidget {
 
 class insertConferenceState extends State<insertConference> {
   String _name;
-  String _email;
-  String _password;
-  String _url;
-  String _phoneNumber;
-  String _calories;
+  String _district;
+  String _dateTime;
+  String _category;
+  String _description;
+  String _website;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildName() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Name'),
+      decoration: new InputDecoration(
+        labelText: "Name",
+        fillColor: Colors.white,
+        border: new OutlineInputBorder(
+          borderRadius: new BorderRadius.circular(10.0),
+          borderSide: new BorderSide(),
+        ),
+      ),
       maxLength: 10,
       validator: (String value) {
         if (value.isEmpty) {
@@ -139,59 +44,156 @@ class insertConferenceState extends State<insertConference> {
   }
 
   Widget _buildDistrict() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'District'),
-      keyboardType: TextInputType.visiblePassword,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Password is Required';
+    return DropDownFormField(
+      hintText: 'Choose a district',
+      onSaved: (value) {
+        setState(() {
+          _district = value;
+        });
+      },
+      onChanged: (value) {
+        setState(() {
+          _district = value;
+        });
+      },
+      validator: (value) {
+        if (value == null) {
+          return 'Please choose a district';
         }
-
         return null;
       },
-      onSaved: (String value) {
-        _password = value;
-      },
+      dataSource: [
+        {
+          "display": "Porto",
+          "value": "Porto",
+        },
+        {
+          "display": "Aveiro",
+          "value": "Aveiro",
+        },
+        {
+          "display": "Lisboa",
+          "value": "Lisboa",
+        },
+        {
+          "display": "Viana do Castelo",
+          "value": "Viana do Castelo",
+        },
+        {
+          "display": "Faro",
+          "value": "Faro",
+        },
+      ],
+      textField: 'display',
+      valueField: 'value',
     );
   }
 
   Widget _buildDate() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Date'),
-      keyboardType: TextInputType.visiblePassword,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Password is Required';
-        }
-
-        return null;
-      },
-      onSaved: (String value) {
-        _password = value;
-      },
-    );
+    return Stack(children: [
+      MaterialButton(
+          minWidth: 100,
+          color: Colors.blueGrey[50],
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          onPressed: () async {
+            showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2222))
+                .then((date) {
+              setState(() {
+                _dateTime = date.toString();
+              });
+            });
+          },
+          child: Text(
+            "Date",
+            textAlign: TextAlign.center,
+          )),
+      Container(
+        decoration: BoxDecoration(
+            borderRadius: new BorderRadius.circular(10.0),
+            border: Border.all(width: 0.5)),
+        width: 345.0,
+        height: 40.0,
+        child: Text(
+            _dateTime == null ? 'Must Pick a date' : _dateTime.substring(0, 10),
+            textAlign: TextAlign.center),
+        alignment: Alignment.center,
+      ),
+    ]);
   }
 
   Widget _buildCategory() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Category'),
-      keyboardType: TextInputType.url,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'URL is Required';
+    return DropDownFormField(
+      hintText: 'Categoty',
+      onSaved: (value) {
+        setState(() {
+          _category = value;
+        });
+      },
+      onChanged: (value) {
+        setState(() {
+          _category = value;
+        });
+      },
+      validator: (value) {
+        if (value == null) {
+          return 'Please choose a category';
         }
-
         return null;
       },
-      onSaved: (String value) {
-        _url = value;
-      },
+      dataSource: [
+        {
+          "display": "Academic Conference",
+          "value": "Academic Conference",
+        },
+        {
+          "display": "Business Conference",
+          "value": "Business Conference",
+        },
+        {
+          "display": "Cultural Conference",
+          "value": "Cultural Conference",
+        },
+        {
+          "display": "Diplomatic Conference",
+          "value": "Diplomatic Conference",
+        },
+        {
+          "display": "Environmental Conference",
+          "value": "Environmental Conference",
+        },
+        {
+          "display": "Journalism Conference",
+          "value": "Journalism Conference",
+        },
+        {
+          "display": "Technology Conference",
+          "value": "Technology Conference",
+        },
+        {
+          "display": "Music Conference",
+          "value": "Music Conference",
+        },
+      ],
+      textField: 'display',
+      valueField: 'value',
     );
   }
 
   Widget _buildDescription() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Description'),
+      decoration: new InputDecoration(
+        labelText: "Description",
+        fillColor: Colors.white,
+        border: new OutlineInputBorder(
+          borderRadius: new BorderRadius.circular(10.0),
+          borderSide: new BorderSide(),
+        ),
+      ),
       keyboardType: TextInputType.phone,
       validator: (String value) {
         if (value.isEmpty) {
@@ -201,26 +203,25 @@ class insertConferenceState extends State<insertConference> {
         return null;
       },
       onSaved: (String value) {
-        _url = value;
+        _description = value;
       },
     );
   }
 
   Widget _buildWebsite() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Website'),
+      decoration: new InputDecoration(
+        labelText: "Website",
+        fillColor: Colors.white,
+        border: new OutlineInputBorder(
+          borderRadius: new BorderRadius.circular(10.0),
+          borderSide: new BorderSide(),
+        ),
+      ),
       keyboardType: TextInputType.number,
-      validator: (String value) {
-        int calories = int.tryParse(value);
-
-        if (calories == null || calories <= 0) {
-          return 'Calories must be greater than 0';
-        }
-
-        return null;
-      },
+      validator: (String value) {},
       onSaved: (String value) {
-        _calories = value;
+        _website = value;
       },
     );
   }
@@ -231,99 +232,101 @@ class insertConferenceState extends State<insertConference> {
       body: Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
-          child: Column(
-        children: <Widget>[
-          Row(children: <Widget>[
-            Stack(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Image.asset('images/pageHeader.png', fit: BoxFit.fill),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(
-                      left: MediaQuery.of(context).size.width * 0.08,
-                      top: MediaQuery.of(context).size.width * 0.08,
-                    ),
-                    child: RaisedButton(
-                      onPressed: () async {
-                        MaterialPageRoute(
-                            builder: (context) => insertConference());
-                      },
-                      color: Color(0xFF6E96EF),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "SIGN OUT",
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
+          child: Column(children: <Widget>[
+        Row(children: <Widget>[
+          Stack(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset('images/pageHeader.png', fit: BoxFit.fill),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.08,
+                    top: MediaQuery.of(context).size.width * 0.08,
+                  ),
+                  child: RaisedButton(
+                    onPressed: () async {
+                      MaterialPageRoute(
+                          builder: (context) => insertConference());
+                    },
+                    color: Color(0xFF6E96EF),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "SIGN OUT",
+                        style: Theme.of(context).textTheme.headline4,
                       ),
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                    )),
+                    ),
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                  )),
+            ],
+          ),
+        ]),
+        Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.08,
+              right: MediaQuery.of(context).size.width * 0.08,
+            ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "InsertConference",
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    alignment: Alignment.topLeft,
+                  )
+                ])),
+        // CONTENT ROW
+        Container(
+          margin: EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildName(),
+                _buildDistrict(),
+                SizedBox(height: 15),
+                _buildDate(),
+                SizedBox(height: 15),
+                _buildCategory(),
+                SizedBox(height: 15),
+                _buildDescription(),
+                SizedBox(height: 15),
+                _buildWebsite(),
+                SizedBox(height: 15),
+                RaisedButton(
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.blue, fontSize: 16),
+                  ),
+                  onPressed: () {
+                    if (!_formKey.currentState.validate()) {
+                      return;
+                    }
+
+                    _formKey.currentState.save();
+
+                    print(_name);
+                    print(_district);
+                    print(_category);
+                    print(_description);
+                    print(_dateTime);
+                    print(_website);
+
+                    //Send to API
+                  },
+                )
               ],
             ),
-          ]),
-          Padding(
-              padding: EdgeInsets.only(
-                left: MediaQuery.of(context).size.width * 0.08,
-                right: MediaQuery.of(context).size.width * 0.08,
-              ),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      child: Text(
-                        "InsertConference",
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                      alignment: Alignment.topLeft,
-                    )
-                  ])),
-          // CONTENT ROW
-          Container(
-            margin: EdgeInsets.all(24),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  _buildName(),
-                  _buildDistrict(),
-                  _buildDate(),
-                  _buildCategory(),
-                  _buildDescription(),
-                  _buildWebsite(),
-                  SizedBox(height: 100),
-                  RaisedButton(
-                    child: Text(
-                      'Submit',
-                      style: TextStyle(color: Colors.blue, fontSize: 16),
-                    ),
-                    onPressed: () {
-                      if (!_formKey.currentState.validate()) {
-                        return;
-                      }
-
-                      _formKey.currentState.save();
-
-                      print(_name);
-                      print(_email);
-                      print(_phoneNumber);
-                      print(_url);
-                      print(_password);
-                      print(_calories);
-
-                      //Send to API
-                    },
-                  )
-                ],
-              ),
-            ),
           ),
-        ],
-      )),
+        ),
+      ])),
     );
   }
 }
