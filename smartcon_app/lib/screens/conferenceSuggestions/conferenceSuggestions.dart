225c4746f053ec5945f59709cfbbb8d5b409dc66
Diana Freitas <up201806230@fe.up.pt>
2020-11-06 12:31:37 +0000

    Correcting bug in import

--- smartcon_app/lib/screens/conferenceSuggestions/conferenceSuggestions.dart
+++ smartcon_app/lib/screens/conferenceSuggestions/conferenceSuggestions.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'file:///C:/Users/diana/open-cx-t2g3-them-d/smartcon_app/lib/screens/conferenceSuggestions/conferenceList.dart';
+import 'package:smartcon_app/screens/conferenceSuggestions/conferenceList.dart';
 import 'package:smartcon_app/models/conference.dart';
 import 'package:smartcon_app/services/database.dart';
 
