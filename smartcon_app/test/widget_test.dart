506bbce8b2791d5e0f9878362b73ac66cf57f536
Mariana Ramos <7marianaramos@gmail.com>
2020-12-15 23:45:19 +0000

    Unit Testings done Closes #22

--- smartcon_app/test/widget_test.dart
+++ smartcon_app/test/widget_test.dart
@@ -6,107 +6,90 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:smartcon_app/models/conference.dart';
+import 'package:smartcon_app/models/session.dart';
 import 'package:smartcon_app/screens/insertConference/conferenceSessions.dart';
 import 'package:smartcon_app/screens/insertConference/insertConference.dart';
 import 'package:smartcon_app/screens/insertConference/newSession.dart';
+import 'package:smartcon_app/screens/insertConference/sessionQuestion.dart';
 
 class MockConf extends Mock implements Conference {}
 
 void main() {
-  group('HomePage', () {
-    testWidgets('Pressing Insert Conference', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: InsertConference()));
-      final insertConferencesPage = find.text("Insert Conference");
-      expect(insertConferencesPage, findsOneWidget);
-      expect(find.byType(TextFormField),
-          findsNWidgets(3)); //Name, Description, Website
-      expect(find.byType(RaisedButton), findsOneWidget); //Next
-      expect(find.byType(MaterialButton), findsOneWidget); //Date
-      expect(find.byType(DropDownFormField),
-          findsNWidgets(2)); //District and Category
-    });
+  testWidgets('Popup if pressing NEXT and form is not filled',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: InsertConference()));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byKey(Key('insert_conference_next')));
+    await tester.pumpAndSettle();
+    final nameError = find.text('Conference data is invalid');
+    await tester.pumpAndSettle();
+    expect(nameError, findsOneWidget);
   });
 
-  group('Insert Conference', () {
-    testWidgets('Errors if pressing NEXT and form is not filled',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: InsertConference()));
-
-      await tester.tap(find.byKey(Key('insert_conference_next')));
-      await tester.pump();
-      final nameError = find.text('Name is Required');
-      final districtError = find.text('Please choose a district');
-      final dateError = find.text('Must Pick a date');
-      final categoryError = find.text('Please choose a category');
-      final descriptiveError = find.text('Description is Required');
-      expect(nameError, findsOneWidget);
-      expect(districtError, findsOneWidget);
-      expect(dateError, findsOneWidget);
-      expect(categoryError, findsOneWidget);
-      expect(descriptiveError, findsOneWidget);
-    });
-
-    testWidgets('Pressing NEXT should go to Conference Sessions',
-        (WidgetTester tester) async {
-      final conf = MockConf();
-
-      await tester.pumpWidget(MaterialApp(home: InsertConference()));
-
-      await tester.enterText(find.byKey(Key('name_field')), 'Conf');
-
-      await tester.tap(find.text('Choose a district'));
-      await tester.tap(find.text("Porto"));
-
-      /*await tester.tap(find.byType(MaterialButton));
-      expect(find.text('December 2020'), findsOneWidget);*/
-
-      await tester.tap(find.text('Description'));
-      await tester.tap(find.text("Technology"));
-
-      /* await tester.enterText(
-          find.byKey(Key('description_field')), 'description');*/
-
-      await tester.pump();
-
-      final confPage =
-          find.text('Insert Conference' /* 'Conference Sessions'*/);
-      expect(confPage, findsOneWidget);
-    });
+  testWidgets('Pressing Insert Conference', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: InsertConference()));
+    final insertConferencesPage = find.text("Insert Conference");
+    expect(insertConferencesPage, findsOneWidget);
+    expect(find.byType(TextFormField),
+        findsNWidgets(3)); //Name, Description, Website
+    expect(find.byType(RaisedButton), findsOneWidget); //Next
+    expect(find.byType(MaterialButton), findsOneWidget); //Date
+    expect(find.byType(DropDownFormField),
+        findsNWidgets(2)); //District and Category
   });
-
   group('Conference Sessions', () {
     /*setUp(
       var model = MockConference();
       await model.initialise();
     );*/
-    testWidgets('Pressing okay symbol', (WidgetTester tester) async {
+    testWidgets('Testing Conference Sessions Screen',
+        (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(home: conferenceSessions()));
+      expect(find.text("Conference Sessions"), findsOneWidget);
+      expect(find.byType(FloatingActionButton), findsNWidgets(2)); //Next
+    });
+    testWidgets('Pressing okay symbol without topis -> error',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: conferenceSessions()));
+      await tester.pumpAndSettle();
       await tester.tap(find.byKey(Key('btn2')));
-      await tester.pump();
-      /*final popupFinder = find.byWidget(AlertDialog);
-      var popupFinderExists = await FlutterDriverUtils.isPresent(world.driver, popupFinder);
-      expectMatch(true, popupFinderExists);*/
-      expect(
-          find.text(
-              'The attendees can now get session suggestions by using this code.'),
-          findsOneWidget);
+      await tester.pumpAndSettle();
+      expect(find.text("You must insert at least one session"), findsOneWidget);
     });
 
     testWidgets('Pressing + symbol', (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(home: conferenceSessions()));
+      await tester.pumpAndSettle();
       await tester.tap(find.byKey(Key('btn1')));
-      //await tester.pump();
-      expect(find.text('New Session'), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect(find.text("New Session"), findsOneWidget);
     });
   });
 
   group('New Session', () {
-    testWidgets('Pressing NEXT shoud go to quiz form',
-        (WidgetTester tester) async {
+    testWidgets('Testing New Session Screen', (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(home: NewSession()));
-      await tester.tap(find.byType(RaisedButton));
       await tester.pump();
       expect(find.text('New Session'), findsOneWidget);
+      expect(find.byType(TextFormField),
+          findsNWidgets(3)); //Name, Description, Website
+      expect(find.byType(RaisedButton), findsNWidgets(2)); //Topics
+      expect(find.byType(MaterialButton), findsNWidgets(2)); //Date, Speakers
+    });
+    testWidgets('Pressing Insert Topics', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: NewSession()));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byKey(Key('topicsButtonn')));
+      await tester.pumpAndSettle();
+      expect(find.text("Insert Topics"), findsOneWidget);
+    });
+
+    testWidgets('Pressing Insert Speakers', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: NewSession()));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byKey(Key('speakerButtonn')));
+      await tester.pumpAndSettle();
+      expect(find.text("Insert Speakers"), findsOneWidget);
     });
   });
 }
